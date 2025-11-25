d1b7b7d186e9e271183d18599b1164927e866371
Melissa Strong <melissastrong89@gmail.com>
2025-11-25 08:59:41 -0500

    fix csrf trusted origins

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
+DJANGO_CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
